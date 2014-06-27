579b395653b69ba0968bae7e7c2665065badafc4
Ramsundar Shandilya <ram.shandilya@ymedialabs.com>
2014-06-27 12:08:16 +0530

    Initial Commit

---
+++ UltraLight/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UltraLight
+//
+//  Created by Ramsundar Shandilya on 27/06/14.
+//  Copyright (c) 2014 Ramsundar Shandilya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RSAppDelegate class]));
+    }
+}
